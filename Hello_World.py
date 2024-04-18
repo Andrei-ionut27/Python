8fd330b598bb6037dde95ff4c93c9a019756a18b
Andrei Florea <andrei175@yahoo.com>
2024-04-18 19:30:17 +0300

    First commit

---
+++ Hello_World.py
@@ -0,0 +1,8 @@
+# comentariiu de one line
+'''
+comentarii multiple line
+linea 2
+linea 3
+'''
+# print printeaza un mesaj in consola
+print('Helo World!')
---
+++ Variabile.py
@@ -0,0 +1,26 @@
+# Variabila = zona din memorie care tine date
+# Variabila = este o cutiuta in care punem valori
+
+# am declarat si initializat variabila marca
+marca_masina = 'Volvo'
+model_masina = 'XC 60'
+
+# nu putem sa punem spatiu in numele variabilei
+# o variabila incepe cu litera mica
+
+# loosely typed - nu trebuie sa specificam tipurile
+
+print(f'Am cumparat o masina marca : {marca_masina}')
+print(f'Este modelul : {model_masina}')
+
+# suprasciere
+model_masina = 'XC 60 facelift'
+
+print(f'Am cumparat o masina marca : {marca_masina}')
+print(f'Este modelul : {model_masina}')
+
+nume = 'Florea'
+prenume = 'Andrei'
+varsta = '32'
+print(prenume + ' ' + nume)
+print(f'{prenume} {nume} si are varsta de {varsta}')
